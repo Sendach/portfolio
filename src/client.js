7e1a195853ee5eb54029ef95c4b4adcef7a42501
sendach <sendachwt@gmail.com>
2022-06-19 14:54:27 +0200

    Changed width of logo on 2000+ monitors

--- src/client.js
+++ src/client.js
@@ -1,8 +1,10 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+console.log('id:', process.env.REACT_APP_SANITY_PROJECT_ID)
+
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'rh1w6b7r',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
