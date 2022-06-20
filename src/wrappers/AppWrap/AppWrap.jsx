d8d2d88b386c7040190a9c8865b83d4802c9787d
sendach <sendachwt@gmail.com>
2022-06-20 03:35:40 +0200

    Changed copyright footer location and cleaned package json

--- src/wrappers/AppWrap/AppWrap.jsx
+++ src/wrappers/AppWrap/AppWrap.jsx
@@ -7,7 +7,7 @@ import './AppWrap.scss';
 const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
     <div id={idName} className={`app__container ${classNames}`}>
-      <div   className="app__wrapper section app__flex">
+      <div className="app__wrapper section app__flex">
         <Component />
       </div>
 
